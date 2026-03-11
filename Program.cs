920ebd2f8e768f04d2d0621a2affdbf6bb2196b0
Данила <prettysgb@gmail.com>
2026-03-11 18:22:48 +0300

    feat: improve battle scenario output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-
 using System;
 
 namespace RPG_Lab_Project
@@ -108,8 +107,10 @@ namespace RPG_Lab_Project
             if (Status == StatusEffect.Shielded)
             {
                 damage -= 5;
-                if (damage < 0) damage = 0;
-                Console.WriteLine(Name + " blocks part of damage with shield");
+                if (damage < 0)
+                    damage = 0;
+
+                Console.WriteLine(Name + " blocks part of damage with shield.");
                 Status = StatusEffect.Normal;
             }
 
@@ -122,7 +123,7 @@ namespace RPG_Lab_Project
 
         public override string ToString()
         {
-            return ClassType + " " + Name + " (HP:" + Health + " MP:" + Mana + ")";
+            return ClassType + " " + Name + " (HP:" + Health + " MP:" + Mana + ", Status:" + Status + ")";
         }
 
         public override bool Equals(object obj)
@@ -140,13 +141,13 @@ namespace RPG_Lab_Project
 
     public class Warrior : Character
     {
-        public Warrior(string name, Random r) : base(name,120,30,20,CharacterClass.Warrior,r){}
+        public Warrior(string name, Random r) : base(name, 120, 30, 20, CharacterClass.Warrior, r) { }
 
         public override void Attack(Character target)
         {
             EnsureAlive();
             int dmg = Damage + rnd.Next(5);
-            Console.WriteLine(Name + " strikes " + target.Name + " with sword");
+            Console.WriteLine(Name + " strikes " + target.Name + " with sword.");
             target.Defend(dmg);
         }
 
@@ -154,7 +155,7 @@ namespace RPG_Lab_Project
         {
             if (Mana < 10)
             {
-                Console.WriteLine(Name + " lacks mana");
+                Console.WriteLine(Name + " lacks mana.");
                 return;
             }
 
@@ -166,12 +167,12 @@ namespace RPG_Lab_Project
 
     public class Mage : Character
     {
-        public Mage(string name, Random r) : base(name,80,100,15,CharacterClass.Mage,r){}
+        public Mage(string name, Random r) : base(name, 80, 100, 15, CharacterClass.Mage, r) { }
 
         public override void Attack(Character target)
         {
             int dmg = Damage + rnd.Next(6);
-            Console.WriteLine(Name + " casts magic bolt at " + target.Name);
+            Console.WriteLine(Name + " casts magic bolt at " + target.Name + ".");
             target.Defend(dmg);
         }
 
@@ -179,7 +180,7 @@ namespace RPG_Lab_Project
         {
             if (Mana < 20)
             {
-                Console.WriteLine(Name + " lacks mana");
+                Console.WriteLine(Name + " lacks mana.");
                 return;
             }
 
@@ -191,12 +192,12 @@ namespace RPG_Lab_Project
 
     public class Archer : Character
     {
-        public Archer(string name, Random r) : base(name,90,40,18,CharacterClass.Archer,r){}
+        public Archer(string name, Random r) : base(name, 90, 40, 18, CharacterClass.Archer, r) { }
 
         public override void Attack(Character target)
         {
             int dmg = Damage + rnd.Next(8);
-            Console.WriteLine(Name + " shoots arrow at " + target.Name);
+            Console.WriteLine(Name + " shoots arrow at " + target.Name + ".");
             target.Defend(dmg);
         }
 
@@ -204,7 +205,7 @@ namespace RPG_Lab_Project
         {
             if (Mana < 15)
             {
-                Console.WriteLine(Name + " lacks mana");
+                Console.WriteLine(Name + " lacks mana.");
                 return;
             }
 
@@ -216,11 +217,11 @@ namespace RPG_Lab_Project
 
     public class Tank : Character
     {
-        public Tank(string name, Random r) : base(name,160,20,12,CharacterClass.Tank,r){}
+        public Tank(string name, Random r) : base(name, 160, 20, 12, CharacterClass.Tank, r) { }
 
         public override void Attack(Character target)
         {
-            Console.WriteLine(Name + " rams " + target.Name);
+            Console.WriteLine(Name + " rams " + target.Name + ".");
             target.Defend(Damage + rnd.Next(4));
         }
 
@@ -240,43 +241,56 @@ namespace RPG_Lab_Project
 
             Character[] party =
             {
-                new Warrior("Thorin",rnd),
-                new Mage("Gandalf",rnd),
-                new Archer("Legolas",rnd),
-                new Tank("Boromir",rnd)
+                new Warrior("Thorin", rnd),
+                new Mage("Gandalf", rnd),
+                new Archer("Legolas", rnd),
+                new Tank("Boromir", rnd)
             };
 
-            Console.WriteLine("=== Characters ===");
+            Loot reward = new Loot("Ancient chest", 150);
+
+            Console.WriteLine("Starting RPG battle simulation...");
+            Console.WriteLine("Possible reward: " + reward);
+
+            Console.WriteLine("\n=== Characters ===");
             foreach (Character c in party)
                 Console.WriteLine(c);
 
             Console.WriteLine("\n=== Battle simulation ===");
 
-            for(int round=1; round<=5; round++)
+            for (int round = 1; round <= 5; round++)
             {
                 Console.WriteLine("\nRound " + round);
 
                 Character attacker = party[rnd.Next(party.Length)];
                 Character target = party[rnd.Next(party.Length)];
 
-                if (attacker == target) continue;
+                if (attacker == target)
+                {
+                    Console.WriteLine("The character decided to skip the turn.");
+                    continue;
+                }
 
                 try
                 {
-                    if (rnd.Next(2)==0)
+                    if (rnd.Next(2) == 0)
                         attacker.Attack(target);
                     else
                         attacker.UseSkill(target);
                 }
-                catch(CharacterDeadException e)
+                catch (CharacterDeadException e)
                 {
                     Console.WriteLine(e.Message);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     Console.WriteLine("Unexpected error: " + e.Message);
                 }
             }
+
+            Console.WriteLine("\n=== Final state ===");
+            foreach (Character c in party)
+                Console.WriteLine(c);
 
             Console.WriteLine("\nSimulation finished.");
             Console.ReadLine();
